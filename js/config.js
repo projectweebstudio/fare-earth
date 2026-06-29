eda4e692cc1595a92c5cbd25f8da2205c86a8355
adminabhangadvt <adminabhangadvt@gmail.com>
2026-06-29 14:57:36 +0530

    urls updated

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    API_URL: "https://script.google.com/macros/s/AKfycbz19D87qF_R2V0UnTPDumzZw4mXKOyv-5VRw_Nz8iFwamjeVajwjtPrT5yZfVdMeDTH/exec",
+    API_URL: "https://script.google.com/macros/s/AKfycbxkowutd-vlaTjv7-cFeKEnJpuxgjHBTJncqZesJxV6pyvm-XfsKfQlxbLbX4Prj66N/exec",
     PRODUCTS_JSON_URL: "products.json",
     TAX_RATE: 8,
     SHIPPING_CHARGE: 10,
